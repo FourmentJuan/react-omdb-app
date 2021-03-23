ea20a6cbb2e6dad1deb68cdff2df81d6dabbd51a
Juan Fourment <fourmentjuan@gmail.com>
2021-03-23 11:03:27 -0300

    Busqueda con error

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,34 +1,67 @@
-import React from 'react'
+import React,{Fragment} from 'react'
+
 import Card from "../components/Card/Card";
 
 
 const API = "http://www.omdbapi.com/?apikey=12645c32";
-
 
 class List extends React.Component {
   constructor() {
     super();
     this.state = {
       data: [],
+      searchTerm:'',
+      error:''
     };
   }
 
   async componentDidMount(){
     const res = await fetch(`${API}&s=batman`);
     const resJSON = await res.json()
-    console.log(resJSON)
     this.setState({
       data: resJSON.Search
     })
   }
+
+  async handleSubmit(e){
+    e.preventDefault()
+    if(!this.state.searchTerm)
+      return this.setState({error:"Please write a valid text"})
+    const res = await fetch(`${API}&s=${this.state.searchTerm}`)
+    const data = await res.json();
+    console
+    if(!data.Search)
+      return this.setState({ error: "There are no results", searchTerm: "" });
+    this.setState({data: data.Search,error:'',searchTerm:''})
+  }
   render() {
-    console.log(this.state.data)
     return (
-      <div className="row">
-        {this.state.data.map((movie) => {
-          return <Card movie={movie} />;
-        })}
-      </div>
+      <Fragment>
+        <div className="row">
+          <div className="col-md-4 offset-md-4 p-4">
+            <form onSubmit={e =>this.handleSubmit(e)}>
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Search"
+                onChange={e => this.setState({
+                  searchTerm:e.target.value
+                })}
+                value={this.state.searchTerm}
+                autoFocus
+              />
+            </form>
+            <p className="text-white">
+              {this.state.error?this.state.error:''}
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          {this.state.data.map((movie,i) => {
+            return <Card movie={movie} key={i}/>;
+          })}
+        </div>
+      </Fragment>
     );
   }
 }
