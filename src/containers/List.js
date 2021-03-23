632ad653cb4b3d8d8602d0ec726b012e2f7bb0d8
Juan Fourment <fourmentjuan@gmail.com>
2021-03-23 09:45:11 -0300

    Conección con la API key

--- src/containers/List.js
+++ src/containers/List.js
@@ -2,35 +2,34 @@ import React from 'react'
 import Card from "../components/Card/Card";
 
 
+const API = "http://www.omdbapi.com/?apikey=12645c32";
 
-class List extends React.Component{
 
-  constructor(){
+class List extends React.Component {
+  constructor() {
     super();
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
   async componentDidMount(){
-    const res = await fetch('../../assets/data.json')
+    const res = await fetch(`${API}&s=batman`);
     const resJSON = await res.json()
     console.log(resJSON)
     this.setState({
-      data: resJSON
+      data: resJSON.Search
     })
-    console.log(`Data: ${this.state.data}`)
   }
-  render(){
-      return(
-          <div className="row">
-          {
-            this.state.data.map(movie => {
-              return <Card movie={movie}/>
-            })
-          }
-        </div>
-      )
+  render() {
+    console.log(this.state.data)
+    return (
+      <div className="row">
+        {this.state.data.map((movie) => {
+          return <Card movie={movie} />;
+        })}
+      </div>
+    );
   }
 }
 
